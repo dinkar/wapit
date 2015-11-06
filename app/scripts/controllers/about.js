fce8fd9d371fb43a306257e80f9731e51b08fb37
Dinkar Pundir <dinkar.pundir@wingify.com>
2015-11-06 20:38:57 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wapitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wapitApp
+ */
+angular.module('wapitApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wapitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wapitApp
+ */
+angular.module('wapitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
