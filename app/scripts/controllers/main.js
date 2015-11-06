c84bb4337837195b0467bbd8c75130fb7242ba2f
Dinkar Pundir <dinkar.pundir@wingify.com>
2015-11-06 23:57:23 +0530

    Added the pushcrew API.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the wapitApp
  */
 angular.module('wapitApp')
-  .controller('MainCtrl', function ($scope, uiGmapGoogleMapApi, uiGmapIsReady, $timeout, $log) {
+  .controller('MainCtrl', function ($scope, uiGmapGoogleMapApi, uiGmapIsReady, $timeout, $log, $window) {
 
     var defaultMapOptions = {
       center: {
@@ -40,6 +40,26 @@ angular.module('wapitApp')
     };
     $scope.mapStatus = 'Loading map.';
 
+    $window._pcq = window._pcq || [];
+    $window._pcq.push(['APIReady', pushCrewAPIReady]);
+    $scope.pcStatus = 'Getting push crew status';
+
+    function pushCrewAPIReady() {
+      $timeout(function(){
+        if($window.pushcrew.subscriberId) {
+          if($window.pushcrew.subscriberId === -1) {
+            $scope.pcStatus = 'Subscriber has blocked push notifications.';
+          } else {
+            $scope.pcStatus = 'Subscriber ID is ' + $window.pushcrew.subscriberId;
+          }
+        } else {
+          $scope.pcStatus = 'User isn\'t a subscriber';
+        }
+
+        console.log('Pushcrew API ready');
+      }, 0);
+    }
+
     function handleLocationError(browserHasGeolocationSupport) {
       $timeout(function () {
         if (browserHasGeolocationSupport) {
@@ -61,13 +81,12 @@ angular.module('wapitApp')
             $scope.mapStatus = 'Location set. Pan the map to set a new center.'
           }, 0);
         }, function () {
-          handleLocationError(true, infoWindow, map.getCenter());
+          handleLocationError(true);
         });
       } else {
-        handleLocationError(false, infoWindow, map.getCenter());
+        handleLocationError(false);
       }
     }
-
 
     uiGmapGoogleMapApi.then(function (mapObj) {
       $scope.mapStatus = 'Map loaded, fetching your location.';
