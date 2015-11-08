6b826a0cdbb7c3c7ca4261525d224ab190b07e2b
Dinkar Pundir <dinkar.pundir@wingify.com>
2015-11-08 23:57:39 +0530

    Changed to grunt serve in app.js

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/dist'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use(express.static(__dirname + '/.tmp'));
 app.listen(port);
