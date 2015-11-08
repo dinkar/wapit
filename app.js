e02b9c4160f9f0a4cff21e11a8f82a08c50546d7
Dinkar Pundir <dinkar.pundir@wingify.com>
2015-11-08 23:14:30 +0530

    Changing app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-var gzippo = require('gzippo');
 var express = require('express');
-var morgan = require('morgan');
+var port = process.env.PORT || 3000;
 var app = express();
-
-app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname + ‘/dist));
+app.listen(port);
